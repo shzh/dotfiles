1b07d8f5cc2b9bcc4b06aa4939935476916e09e3
Shuo Zheng <shuo.s.zheng@gmail.com>
2016-02-07 23:20:48 -0500

    added taskrc

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@
 
 # Set up symbolic links
 DOTFILESPATH=$( cd $(dirname $0)/.. ; pwd -P )
-DOTFILES=(vimrc zshrc tmux.conf spacemacs)
+DOTFILES=(vimrc zshrc tmux.conf spacemacs taskrc)
 
 for dotfile in "${DOTFILES[@]}" ; do
   if [ -L ~/.$dotfile ] ; then
